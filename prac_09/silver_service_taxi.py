48cb9ab1d2b4509fa8be41032f54b8f335949945
Lenovo <shijie.shen@my.jcu.edu.au>
2025-07-27 11:10:09 +0800

    This file develops the SilverServiceTaxi class, which inherits from the Taxi class and adds flagfall functionality.

---
+++ prac_09/silver_service_taxi.py
@@ -0,0 +1,14 @@
+from taxi import Taxi
+import random
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.50
+    def __init__(self,name,fuel,fanciness):
+        super().__init__(name,fuel)
+        self.fanciness =fanciness
+        self.price_per_km*=fanciness
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+    def get_fare(self):
+        return self.flagfall + super().get_fare()
